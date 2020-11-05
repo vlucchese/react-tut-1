1956d2eac39efc0aee1c4cae7ee11457e77dc847
Vincenzo Lucchese <vlucchese@altevie.com>
2020-11-05 18:28:11 +0100

    add two step over of tut

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function SquareColumnRow(props) {
+  return (
+    <button className="squareColumnRow" >
+      {props.value}
+    </button>
+  );
+}
 
 function Square(props) {
   return (
@@ -20,18 +27,6 @@ class Board extends React.Component {
   };
 }
 
-handleClick(i) {
-  const squares = this.state.squares.slice();
-  if (calculateWinner(squares) || squares[i]) {
-    return;
-  }
-  squares[i] = this.state.xIsNext ? 'X' : 'O';
-  this.setState({
-    squares: squares,
-    xIsNext: !this.state.xIsNext,
-  });
-}
-
 renderSquare(i) {
   return (
     <Square
@@ -41,28 +36,47 @@ renderSquare(i) {
   );
 }
 
+renderSquareColumnRow(i) {
+  return (
+    <SquareColumnRow
+      value={i}
+    />
+  );
+}
+
 render() {
     return (
       <div>
         <div className="board-row">
+          {this.renderSquareColumnRow(' ')}
+          {this.renderSquareColumnRow('a')}
+          {this.renderSquareColumnRow('b')}
+          {this.renderSquareColumnRow('c')}
+          {this.renderSquareColumnRow('d')}
+        </div>
+        <div className="board-row">
+          {this.renderSquareColumnRow('1')}
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
           {this.renderSquare(3)}
         </div>
         <div className="board-row">
+        {this.renderSquareColumnRow(2)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
           {this.renderSquare(6)}
           {this.renderSquare(7)}
         </div>
         <div className="board-row">
+        {this.renderSquareColumnRow(3)}
           {this.renderSquare(8)}
           {this.renderSquare(9)}
           {this.renderSquare(10)}
           {this.renderSquare(11)}
         </div>
         <div className="board-row">
+        {this.renderSquareColumnRow(4)}
           {this.renderSquare(12)}
           {this.renderSquare(13)}
           {this.renderSquare(14)}
@@ -83,6 +97,7 @@ class Game extends React.Component {
       }],
       xIsNext: true,
       stepNumber: 0,
+      buttonClicked: Array(16).fill(null),
     };
   }
 
@@ -90,16 +105,21 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    const buttonClicked = this.state.buttonClicked.slice();
+
     if (calculateWinner(squares) || squares[i]) {
+      console.log(squares[i]);
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    buttonClicked[this.state.stepNumber] = i;
     this.setState({
       history: history.concat([{
         squares: squares
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+      buttonClicked : buttonClicked,
     });
   }
 
@@ -117,7 +137,11 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move
+        + ' '
+        + history[move].squares[this.state.buttonClicked[move-1]]
+        + ' in '
+        + calculateColRow(this.state.buttonClicked[move-1]) :
         'Go to game start';
       return (
         <li key={move}>
@@ -156,6 +180,27 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateColRow(square_select) {
+    var myMap = new Map();
+    myMap.set(0, 'a-1');
+    myMap.set(1, 'b-1');
+    myMap.set(2, 'c-1');
+    myMap.set(3, 'd-1');
+    myMap.set(4, 'a-2');
+    myMap.set(5, 'b-2');
+    myMap.set(6, 'c-2');
+    myMap.set(7, 'd-2');
+    myMap.set(8, 'a-3');
+    myMap.set(9, 'b-3');
+    myMap.set(10, 'c-3');
+    myMap.set(11, 'd-3');
+    myMap.set(12, 'a-4');
+    myMap.set(13, 'b-4');
+    myMap.set(14, 'c-4');
+    myMap.set(15, 'd-4');
+  return myMap.get(square_select);
+}
 
 
 function calculateWinner(squares) {
